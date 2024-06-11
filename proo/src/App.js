b883e2dae4ff3039d913d406cf3850693be6a0b8
shehanminura <shehanminuradp@gmail.com>
2024-06-11 19:33:55 +0530

    learn loop component

--- proo/src/App.js
+++ proo/src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
 import Main from './Components/Main';
 
+const myData = [
+  {
+    name:'shehan',
+    city:'galle',
+    position:'Web Developer'
+  },
+  {
+    name:'hasaranga',
+    city:'mathara',
+    position:'App Developer'
+  },
+  {
+    name:'achine',
+    city:'Kandy',
+    position:'React Developer'
+  },
+  {
+    name:'kaushi',
+    city:'colombo',
+    position:'Java Developer'
+  },
+]
+
 function App() {
   return (
+    <>
     <div>
       SMH React
-      <Main name='Minura' city='Kandy' position='Java Developer'>
-      
-      </Main>
-      <Main name='Hansana' city='Galle' position='Python Developer'/>
-      <Main name='Kaushi' city='Colombo' position='APP Developer'/>
+     <h1>Shehan Minura</h1>
     </div>
+
+    <p>DP Codeing</p>
+
+    {myData.map((ele) => {
+      return <p> <Main name={ele.name} city={ele.city} position={ele.position} /> </p>
+
+    })}
+    </>
   );
 }
 
--- proo/src/Components/Main.js
+++ proo/src/Components/Main.js
@@ -12,19 +12,19 @@
 
 
 
-export default function Main(props) {
+export default function Main(ele) {
   return (
     <div>
       <h1>Main</h1>
       <ul>
-        <li>{props.name}</li>
-        <li>{props.position}</li>
-        <li>{props.city}</li>
+        <li>{ele.name}</li>
+        <li>{ele.position}</li>
+        <li>{ele.city}</li>
         <li>React</li>
         <li>Node</li>
         <li>Express</li>
       </ul>
-      {props.children}
+      
     </div>
   );
 }
